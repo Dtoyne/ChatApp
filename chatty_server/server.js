4a1e073302dbe376f268a302471d873533a5681f
Dylan Toyne <dylantoyne@gmail.com>
2017-02-14 21:46:42 +0000

    uploading unfinished project from desktop. Latptop with most of the final work is down at the moment but will upload project in its completion asap.

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const uuid = require('node-uuid');
-
-// Set the port to 8080
 const PORT = 8080;
 
 // Create a new express server
@@ -14,52 +12,49 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
-let counter = 0;
-
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
-  // Increment the number of people online
-  counter++;
-  // Broadcast to everyone the number of people online
-  wss.broadcast(JSON.stringify({type: "counter", count: counter}))
-
-  ws.on('message', (message) => {
-    const data = JSON.parse(message);
-
-    data.id = uuid.v4();
-
-    switch (data.type) {
-      case "postMessage":
-        data.type = "incomingMessage"
-        wss.broadcast(JSON.stringify(data));
-      break;
-
-      case "postNotification":
-        data.type = "incomingNotification"
-        wss.broadcast(JSON.stringify(data));
-      break;
-
-      default:
-        throw new Error("Unknown event type " + data.type);
-    }
-
+  wss.broadcast({
+    type: 'counter',
+    count: wss.clients.length
   });
 
+  ws.on('message', handleMessage);
+
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => {
-      console.log('Client disconnected')
-      // Decrement the counter of current users
-      counter--;
-      //Broadcast the current amount of users online to everyone connected
-      wss.broadcast(JSON.stringify({type: "counter", count: counter}))
+    console.log('Client disconnected')
+    wss.broadcast({
+      type: 'counter',
+      count: wss.clients.length
     });
   });
 
+let counter = 0;
+
 wss.broadcast = (data) => {
-  wss.clients.forEach( function each(client) {
-    client.send(data);
+  console.log('Broadcasting: ', data)
+  wss.clients.forEach((client) => {
+    client.send(JSON.stringify(data));
   });
-};
+  counter++;
+  console.log(counter);
+}
+
+function handleMessage(message) {
+  message = JSON.parse(message);
+  console.log('Received message: ', message);
+  if (message.type === 'postMessage') {
+     message.type = 'incomingMessage';
+     message.id = uuid.v4();
+  } else if (message.type === 'postNotification') {
+     message.type = 'incomingNotification';
+     message.id = uuid.v4();
+  }
+  wss.broadcast(message);
+  }
+});
